29611742f2ae06fce9f957afce74ed362f1462cb
Carson <carsonjohnbain@gmail.com>
2024-02-04 15:21:21 -0800

    logic for placing X and O complete

--- script.js
+++ script.js
@@ -7,10 +7,25 @@ const Gameboard = (() => {
             boardHtml += `<div class="square" id="square-${index}">${square}</div>`
         })
         document.querySelector('#gameboard').innerHTML = boardHtml;
+        const squares = document.querySelectorAll('.square');
+        squares.forEach((square) => {
+            square.addEventListener('click', Game.handleClick);
+        })
+    }
+
+    function update(index, value) {
+        gameboard[index] = value;
+        render();
+    }
+
+    function getGameBoard() {
+       return gameboard;
     }
 
     return {
-        render
+        render,
+        update,
+        getGameBoard
     }
 })();
 
@@ -38,10 +53,23 @@ const Game = (() => {
         currentPlayerIndex = 0;
         gameOver = false;
         Gameboard.render();
+        const squares = document.querySelectorAll('.square');
+        squares.forEach((square) => {
+            square.addEventListener('click', handleClick);
+        })
     }
     
+    function handleClick(event) {
+       let index = +event.target.id.split('-')[1];
+       if(Gameboard.getGameBoard()[index] !== '') {
+        return;
+       }
+       Gameboard.update(index, players[currentPlayerIndex].mark)
+       currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
+    }
     return {
         start,
+        handleClick
     }
 })();
 
